6be2d1a153e595d91f8311eaaefb15dacd8fca20
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-09-16 10:08:22 +0200

    Tarea02

---
+++ Tarea02/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Properties;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Muestra la memoria total, libre y en uso, exrpesado en MiB
+        Runtime rt = Runtime.getRuntime();
+        long memoriaTotal = rt.totalMemory(); // Memoria total
+        System.out.println("Memoria total: " + memoriaTotal/1024 + " Mib");
+
+        long memoriaLibre = rt.freeMemory();
+        System.out.println("Memoria libre: " + memoriaLibre/1024+ " Mib");
+
+        // Memoria en uso
+        System.out.println("Memoria en uso: " + (memoriaTotal - memoriaLibre) / 1024 + "MiB");
+
+        System.out.println("------------------------------------------------------------------------");
+        // Muesrta el número de procesadores dispoblis para la JVM
+        int procesasdoresDisponibles = Runtime.getRuntime().availableProcessors();
+        System.out.println("Número de procesadores disponibles: " + procesasdoresDisponibles );
+
+        System.out.println("-------------------------------------------------------------------------");
+        // Propiedades de la clase System
+        Properties propiedades = System.getProperties();
+        System.out.println(propiedades);
+
+
+    }
+
+
+}
