b2e39077bb98e0d8e591d96acd54c050772f9aca
DanielRTato <drodrigueztato@danielcastelao.org>
2025-09-16 19:34:20 +0200

    falta comprobar si funciona

--- Tarea03/src/Main.java
+++ Tarea03/src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Pide al usuario un nombre o ruta de archivo, si existe lo abre con un editor de texto, si no lo crea.
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce la ruta o el nombre del archivo que deseas abrir: ");
+        String documento = teclado.nextLine();
+
+        File archivo = new File(documento);
+
+        boolean esWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+        String editor = "";
+
+        if (esWindows) {
+            editor ="notepad";
+        } else {
+            editor = "gedit";
         }
+
+        ProcessBuilder pbuilder = new ProcessBuilder(editor, archivo.getAbsolutePath());
+        try {
+            pbuilder.start();
+        }catch (IOException e) {
+            System.out.println("Error"+e);
+        }
+
+
     }
 }
