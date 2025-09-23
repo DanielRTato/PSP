1bc57700116f77c052bff19c42b1f73950e9f766
DanielRTato <drodrigueztato@danielcastelao.org>
2025-09-23 19:10:01 +0200

    Hacer bien la conversion a Mib

--- Tarea02/src/Main.java
+++ Tarea02/src/Main.java
@@ -1,20 +1,18 @@
 import java.util.Properties;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         //Muestra la memoria total, libre y en uso, exrpesado en MiB
         Runtime rt = Runtime.getRuntime();
         long memoriaTotal = rt.totalMemory(); // Memoria total
-        System.out.println("Memoria total: " + memoriaTotal/1024 + " Mib");
+        System.out.println("Memoria total: " + memoriaTotal / (1024 * 1024) + " Mib");
 
         long memoriaLibre = rt.freeMemory();
-        System.out.println("Memoria libre: " + memoriaLibre/1024+ " Mib");
+        System.out.println("Memoria libre: " + memoriaLibre/ (1024 * 1024) + " Mib");
 
         // Memoria en uso
-        System.out.println("Memoria en uso: " + (memoriaTotal - memoriaLibre) / 1024 + "MiB");
+        System.out.println("Memoria en uso: " + (memoriaTotal - memoriaLibre) / (1024 * 1024) + "MiB");
 
         System.out.println("------------------------------------------------------------------------");
         // Muesrta el número de procesadores dispoblis para la JVM
@@ -24,7 +22,7 @@ public class Main {
         System.out.println("-------------------------------------------------------------------------");
         // Propiedades de la clase System
         Properties propiedades = System.getProperties();
-        System.out.println(propiedades);
+        propiedades.list(System.out); // Con formato clave valor automaticamente
 
 
     }
