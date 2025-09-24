d8d3eb9008bc0f8343f12781a98928641137a5f0
DanielRTato <drodrigueztato@danielcastelao.org>
2025-09-24 20:37:14 +0200

    Añadidos logs

--- Tarea06/src/Lanzador.java
+++ Tarea06/src/Lanzador.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Lanzador {
 
@@ -11,22 +9,33 @@ public class Lanzador {
     public static void hacerPing (String host) {
         String[] comando = {"ping", "-n", "4", host};
         ProcessBuilder pb = new ProcessBuilder(comando);
+        File logGood = new File("ping_output.log");
+        File logError = new File("ping_error.log");
 
         try {
          //pb.inheritIO();
          Process p = pb.start();
          int exitCode =  p.waitFor();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedWriter bwError = new BufferedWriter(new FileWriter(logError, true));
+            BufferedWriter bwOk = new BufferedWriter(new FileWriter(logGood, true));
+           // System.out.println(logGood.isFile());
             String linea = "";
 
             while ((linea = br.readLine()) != null) {
                 if (linea.contains(("no pudo encontrar")) || linea.contains("vuelva a intentarlo")){
                     System.out.println("[ERROR] " + linea);
+                    bwError.write("[ERROR] " + linea);
+                    bwError.newLine();
                 } else {
                     System.out.println("[OK] " + linea);
-                }
+                    bwOk.write("[OK] " + linea);
+                    bwOk.newLine();
 
+                }
             }
+            bwError.close();
+            bwOk.close();
 
          System.out.println("Operación completada. Codigo de salida: " + exitCode);
 
