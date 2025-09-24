5e1ac22eb2e7893b54cec0563d4c2c52037969de
DanielRTato <drodrigueztato@danielcastelao.org>
2025-09-24 20:19:40 +0200

    lvl2 terminado

--- Tarea06/src/Lanzador.java
+++ Tarea06/src/Lanzador.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Lanzador {
 
@@ -11,15 +13,25 @@ public class Lanzador {
         ProcessBuilder pb = new ProcessBuilder(comando);
 
         try {
-         pb.inheritIO();
+         //pb.inheritIO();
          Process p = pb.start();
          int exitCode =  p.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String linea = "";
+
+            while ((linea = br.readLine()) != null) {
+                if (linea.contains(("no pudo encontrar")) || linea.contains("vuelva a intentarlo")){
+                    System.out.println("[ERROR] " + linea);
+                } else {
+                    System.out.println("[OK] " + linea);
+                }
+
+            }
 
          System.out.println("Operación completada. Codigo de salida: " + exitCode);
 
         } catch (IOException | InterruptedException e) {
             System.out.println("Error " + e);
-
         }
     }
 }
