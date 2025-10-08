cce43a11b3ede225c400bf9f465144c44e0af4cb
DanielRTato <drodrigueztato@danielcastelao.org>
2025-10-08 09:50:45 +0200

    Tarea 13

--- Tarea13/src/Hilo.java
+++ Tarea13/src/Hilo.java
@@ -1,14 +1,25 @@
 public class Hilo extends Thread{
-    String nome;
 
-    public Hilo (String nome) {
-        this.nome = nome;
+
+    public Hilo (String nome, int prioridad) {
+        setName(nome);
+        setPriority(prioridad);
     }
 
     @Override
     public void run() {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(nome + " número de iteración: " + i);
+        int max = 10000;
+        int min = 1000;
+
+
+        for (int i = 1; i <= 10; i++) {
+            int tiempo = (int)(Math.random() * (max - min + 1)) + min;
+            System.out.println(getName() + " número de iteración: " + i + " prioridad: " + getPriority());
+            try {
+                Hilo.sleep(tiempo);
+            } catch (InterruptedException e) {
+                System.out.println("Error en sleep " + e);
+            }
         }
     }
 
--- Tarea13/src/Main.java
+++ Tarea13/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Hilo hilo1 = new Hilo("Hilo1");
+        Hilo hilo1 = new Hilo("Hilo1",Thread.MIN_PRIORITY);
+        Hilo hilo2 = new Hilo("Hilo2",Thread.NORM_PRIORITY);
+        Hilo hilo3 = new Hilo("Hilo3",5);
+        Hilo hilo4 = new Hilo("Hilo4",Thread.MAX_PRIORITY);
 
         hilo1.start();
-        hilo1.interrupt();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
 
     }
 }
